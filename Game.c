49a7811c1c3715dc7baced9de7540bdc175844f9
caleb0 <caivecode@gmail.com>
2016-05-04 16:34:41 +0800

    4/5/16

--- Game.c
+++ Game.c
@@ -1,4 +1,7 @@
-#include "Game.h"
+#include "game.h"
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
 #define NUM_STUDENT 6
 
 typedef struct _vector {
@@ -6,6 +9,13 @@ typedef struct _vector {
     double y;
 }Vector2; //A Vector struct for storing coordinates
 
+typedef struct _game * Game;
+
+int main(int argc, char* argv[]){
+    printf("Started game\n");
+    makeGame();
+    return EXIT_SUCCESS;
+}
 typedef struct _game {
     //Region information
     int regionD[NUM_REGIONS]; //Creates an int for every region that represents a discipline
@@ -26,118 +36,131 @@ typedef struct _game {
     int pNumIP[NUM_UNIS]; //Value of IPs for each player
     int pStudents[NUM_UNIS][NUM_STUDENT]; //Value of student types for each player
     int pPublication[NUM_UNIS]; //Value of publications for each player
-	int pARC[NUM_UNIS]; //Value for ARCS for each player
+    int pARC[NUM_UNIS]; //Value for ARCS for each player
     
     //Leaderboard information
     int topKPI; //Stores the top KPI value
     int topPublication; //Stores the top publication value
-} game;
+}game;
 
-Game newGame() {
-	Game g = malloc(sizeof(struct _game));//malloc memory for the whole game
-	assert(g != NULL);
-	g->whoseTurn = -1; //terra nullis turn
-	g->topKPI = NO_ONE;
-	g->topPublication = NO_ONE;
-	createVertex(g);
-	return g;
+
+/*void vertexSetToZeroZero(struct Vector2 * v) {
+    v->y = 0;
+    v->x = 0;
+}*/
+
+Game makeGame() {
+    printf("Making a new game\n");
+    Game g = malloc(sizeof(struct _game));
+    printf("Made new game\n");//malloc memory for the whole game
+    assert(g != NULL);
+    g->whoseTurn = -1; //terra nullis turn
+    g->topKPI = NO_ONE;
+    g->topPublication = NO_ONE;
+    printf("%g\n",g->vertices[0][0]);
+
+    //vertexSetToZeroZero(g->vertices[0][0]);
+    return g;
 }
+
 
 
 void createVertex(Game g) {
-	int currentVertexX = 0;
-	int currentVertexY = 0;
-	int currentTile = 0;
-	int currentVertex;
-	while (currentTile < NUM_REGIONS) {
-		currentVertex = 0;
-		while (currentVertex < 6) {
-			if (currentTile < 3) {
-				if (currentVertex < 3) {
-					g->vertices[currentTile][currentVertex].y = 1;
-					g->vertices[currentTile][currentVertex].x = (currentVertex + 2) + 2 * currentTile;
-				}
-				else {
-					g->vertices[currentTile][currentVertex].y = 0;
-					g->vertices[currentTile][currentVertex].x = (currentVertex - 3) + 2 * currentTile;
-				}
-			}
-			else if (currentTile < 7) {
-				if (currentVertex < 3) {
-					g->vertices[currentTile][currentVertex].y = 2;
-					g->vertices[currentTile][currentVertex].x = (currentVertex + 1) + 2 * (currentTile - 3);
-				}
-				else {
-					g->vertices[currentTile][currentVertex].y = 1;
-					g->vertices[currentTile][currentVertex].x = (currentVertex - 4) + 2 * (currentTile - 3);
-				}
-			}
-			else if (currentTile < 12) {
-				if (currentVertex < 3) {
-					g->vertices[currentTile][currentVertex].y = 3;
-					g->vertices[currentTile][currentVertex].x = currentVertex + 2 * (currentTile - 7);
-				}
-				else {
-					g->vertices[currentTile][currentVertex].y = 2;
-					g->vertices[currentTile][currentVertex].x = (currentVertex - 5) + 2 * (currentTile - 7);
-				}
-			}
-			else if (currentTile < 16) {
-				if (currentVertex < 3) {
-					g->vertices[currentTile][currentVertex].y = 4;
-					g->vertices[currentTile][currentVertex].x = (currentVertex + 1) + 2 * (currentTile - 12);
-				}
-				else {
-					g->vertices[currentTile][currentVertex].y = 3;
-					g->vertices[currentTile][currentVertex].x = (currentVertex - 4) + 2 * (currentTile - 12);
-				}
-			}
-			else if (currentTile < 19) {
-				if (currentVertex < 3) {
-					g->vertices[currentTile][currentVertex].y = 5;
-					g->vertices[currentTile][currentVertex].x = (currentVertex + 2) + 2 * (currentTile - 16);
-				}
-				else {
-					g->vertices[currentTile][currentVertex].y = 4;
-					g->vertices[currentTile][currentVertex].x = (currentVertex - 3) + 2 * (currentTile - 16);
-				}
-			}
-		}
-	}
+
+    int currentTile = 0;
+    int currentVertex;
+    while (currentTile < NUM_REGIONS) {
+        currentVertex = 0;
+        while (currentVertex < 6) {
+            if (currentTile < 3) {
+                if (currentVertex < 3) {
+                    g->vertices[currentTile][currentVertex]->y = 1;
+                    g->vertices[currentTile][currentVertex]->x = (currentVertex + 2) + 2 * currentTile;
+                }
+                else {
+                    g->vertices[currentTile][currentVertex]->y = 0;
+                    g->vertices[currentTile][currentVertex]->x = (currentVertex - 3) + 2 * currentTile;
+                }
+            }
+            else if (currentTile < 7) {
+                if (currentVertex < 3) {
+                    g->vertices[currentTile][currentVertex]->y = 2;
+                    g->vertices[currentTile][currentVertex]->x = (currentVertex + 1) + 2 * (currentTile - 3);
+                }
+                else {
+                    g->vertices[currentTile][currentVertex]->y = 1;
+                    g->vertices[currentTile][currentVertex]->x = (currentVertex - 4) + 2 * (currentTile - 3);
+                }
+            }
+            else if (currentTile < 12) {
+                if (currentVertex < 3) {
+                    g->vertices[currentTile][currentVertex]->y = 3;
+                    g->vertices[currentTile][currentVertex]->x = currentVertex + 2 * (currentTile - 7);
+                }
+                else {
+                    g->vertices[currentTile][currentVertex]->y = 2;
+                    g->vertices[currentTile][currentVertex]->x = (currentVertex - 5) + 2 * (currentTile - 7);
+                }
+            }
+            else if (currentTile < 16) {
+                if (currentVertex < 3) {
+                    g->vertices[currentTile][currentVertex]->y = 4;
+                    g->vertices[currentTile][currentVertex]->x = (currentVertex + 1) + 2 * (currentTile - 12);
+                }
+                else {
+                    g->vertices[currentTile][currentVertex]->y = 3;
+                    g->vertices[currentTile][currentVertex]->x = (currentVertex - 4) + 2 * (currentTile - 12);
+                }
+            }
+            else if (currentTile < 19) {
+                if (currentVertex < 3) {
+                    g->vertices[currentTile][currentVertex]->y = 5;
+                    g->vertices[currentTile][currentVertex]->x = (currentVertex + 2) + 2 * (currentTile - 16);
+                }
+                else {
+                    g->vertices[currentTile][currentVertex]->y = 4;
+                    g->vertices[currentTile][currentVertex]->x = (currentVertex - 3) + 2 * (currentTile - 16);
+                }
+            }
+            currentVertex++;
+        }
+        currentTile++;
+    }
 }
 void disposeGame(Game g) {
-	free(g);
+    free(g);
 }
 int getCampus(Game g, path pathToVertex) {
-	return 0;
+    return 0;
 }
 
 int getKPIpoints(Game g, int player) {
-	return g->pKPI[player];
+    return g->pKPI[player];
 }
 int getARCs(Game g, int player) {
-	return g->pARC[player];
+    return g->pARC[player];
 }
 int getG08s(Game g, int player) {
-	return g->pNumGO8[player];
+    return g->pNumGO8[player];
 }
 
 int getCampuses(Game g, int player) {
-	return g->pNumCampus[player];
+    return g->pNumCampus[player];
 }
 
 int getIPs(Game g, int player) {
-	return g->pNumIP[player];
+    return g->pNumIP[player];
 }
 
 int getPublications(Game g, int player) {
-	return g->pPublication[player];
+    return g->pPublication[player];
 }
 
 int getStudents(Game g, int player, int discipline) {
-	return g->pStudents[player][discipline];
+    return g->pStudents[player][discipline];
 }
 
 int getExchangeRate(Game g, int player, int disciplineFrom, int disciplineTo) {
-	return 0;
+    return 0;
 }
+
