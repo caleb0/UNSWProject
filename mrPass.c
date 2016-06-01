51f7da647aed55d1e14d7a34c955c595902285aa
caleb0 <caivecode@gmail.com>
2016-06-01 17:56:21 +0800

    this does not work.

--- mrPass.c
+++ mrPass.c
@@ -22,11 +22,51 @@ action decideAction (Game g) {
     int mJ = getStudents (g, player, STUDENT_MJ);
     int mTV = getStudents (g, player, STUDENT_MTV);
     int mMoney = getStudents(g, player, STUDENT_MMONEY);
+    int mHTW = getStudents(g, player, STUDENT_BQN);
+    int mBPS = getStudents(g, player, STUDENT_BPS);
+    
     if ((mJ > 0) && (mTV > 0) && (mMoney > 0)) {
         nextAction.actionCode = START_SPINOFF;
+    } else if ((mHTW > 0) && (mBPS > 0)){
+        char[PATH_LIMIT] myPath = [];
+        int counter = 0;
+        while (counter < 150){
+        }
+        
+        for (int i = 0; i < 150; i++){
+            myPath[i] = 'L';
+            
+            
+            if (((getARC(g, myPath)) == VACANT_ARC)){
+                for (int a = 1; a < 10; a++){
+                    myPath[i + a] = 'L';
+                }
+            }
+            
+            
+            myPath[i] = 'R';
+            if (!((getARC(g, myPath)) == VACANT_ARC)){
+                continue;
+            }
+            myPath[i] = 'B';
+            if (!((getARC(g, myPath)) == VACANT_ARC)){
+                continue;
+            }
+        }
+        
     } else {
         nextAction.actionCode = PASS;
     }
     
     return nextAction;
+}
+
+char[] randomPath(){
+    bool validPath = false;
+    char[PATH_LIMIT] myPath = [];
+    int counter = 0;
+    
+    while (!validPath && counter < PATH_LIMIT){
+        
+    }
 }
